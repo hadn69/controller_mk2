4783a8dd67d33dacba3c44fdd6fa33e73ab0ab7d
Hayden69 <hayden@hrznstudio.com>
2019-09-18 18:25:04 -0700

    comments and being dumb

--- controller_mk2.ino
+++ controller_mk2.ino
@@ -14,7 +14,7 @@ double currentArray[20];
 
 //devices
 ACS712 sensor(ACS712_30A, A15);
-CytronMD motor(PWM_DIR, 3, 4); // PWM = Pin 3, DIR = Pin 4.
+CytronMD motor(PWM_DIR, 3, 2); // PWM = Pin 3, DIR = Pin 2.
 LedPrintJustifiable myLed = LedPrintJustifiable(
     22, // DATA PIN
     24, // CLOCK PIN
@@ -64,20 +64,26 @@ void loop()
 
 void run(int speed, int dir)
 {
-  if (abs(tempSpeed - speed) > 20)
+  //motor control
+  if (abs(tempSpeed - speed) > 30)
   {
+    //get major speed change
     tempSpeed = speed;
   }
   else if (speed < 30)
   {
+    //reasonable 0 point?
     tempSpeed = 0;
     speed = 0;
   }
   else
   {
+    //maintain last speed
     speed = tempSpeed;
   }
+  //adjust range
   int temp = (speed / 4);
+  // why no fucking .dir you dumb library
   if (dir)
   {
     motor.setSpeed(temp);
